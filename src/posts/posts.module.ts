35ae6022a31b57421a72ef072e40179485b0c32f
rreyes2025 <rreyes2025@alu.uct.cl>
2026-05-05 23:32:00 -0400

    Delete src/posts/posts.module.ts

--- src/posts/posts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common"
-import { PostsController } from "@/posts/posts.controller"
-import { PostsService } from "@/posts/posts.service"
-
-@Module({
-    controllers: [PostsController],
-    providers: [PostsService],
-})
-export class PostsModule {}
